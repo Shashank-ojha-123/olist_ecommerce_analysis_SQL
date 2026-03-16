f0ff162be85e205b75b5a87333aad3c11473ef3e
Shashank Ojha <158760578+Shashank-ojha-123@users.noreply.github.com>
2026-03-16 09:22:36 +0530

    Delete Running-purchase-total-datewise.sql

--- Running-purchase-total-datewise.sql
+++
@@ -1,6 +0,0 @@
-select p.Payment_type ,p.payment_value,
-order_purchase_timestamp,
- sum(Payment_value) over( order by o.order_purchase_timestamp) 
- from olist_order_payments p
- full join olist_orders o
- on p.order_id=o.order_id
